d7b9a7318beb246946056de845d1c81be9f58209
Petar Zigic <zigic@outlook.com>
2021-03-26 17:35:39 -0400

    Server station endpoint and cors

--- server/app.js
+++ server/app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+
+var allowedOrigins = ['http://localhost:5001',
+                      'https://localhost:5001'];
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,6 +19,9 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5001'
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const csv = require('csv-parser')
+const fs = require('fs')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/status', function(req, res, next) {
+  try {
+  return res.send('Up and running');
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+/* GET home page. */
+router.get('/stations', function(req, res, next) {
+  try {
+    let results = [];
+    fs.createReadStream('stations/Stations_2019.csv')
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+      console.log(results);
+      res.send(results);
+    });
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
 });
 
 module.exports = router;
